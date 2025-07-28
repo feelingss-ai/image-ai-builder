36b3469c79bbe87700194a19f54275e8f14a6c7c
Ada Lui <luitszhin7@gmail.com>
2025-07-28 15:56:08 +0800

    ui: add checkbox and align layout using ion-grid

--- server/app/pages/review-annotation.tsx
+++ server/app/pages/review-annotation.tsx
@@ -49,6 +49,14 @@ let style = Style(/* css */ `
   padding: 0.5rem 0;
   font-size: 1.2rem;
 }
+
+.image-item img {
+  width: 100%;
+  height: 100px;
+  object-fit: contain;
+  padding: 0.5rem;
+}
+
 `)
 
 let script = Script(/* js */ `
@@ -164,10 +172,15 @@ function ImageItem(attrs: {
   user: User | null
 }) {
   return (
-    <div class="image-item">
-      <img src={attrs.image_url} />
-      <div class="image-item--filename">{attrs.filename}</div>
-    </div>
+    <ion-item class="image-item-container">
+      <ion-checkbox></ion-checkbox>
+      <div class="image-item">
+        <img src={attrs.image_url} />
+        <div class="image-item--filename" style="text-align: center;">
+          {attrs.filename}
+        </div>
+      </div>
+    </ion-item>
   )
 }
 
@@ -282,60 +295,62 @@ function Main(attrs: {}, context: DynamicContext) {
       <br />
       <ion-segment-view>
         <ion-segment-content id="content-yes">
-          {mapArray(
-            get_image_filename_by_id_array(
-              get_image_by_label_id_answer
-                .all({
-                  label_id,
-                  answer: stringToNumber('yes'),
-                })
-                .flat(),
-            ),
-            filename => {
-              return (
-                <ImageItem
-                  image_url={filename.filename}
-                  filename={filename.original_filename}
-                  user={user}
-                />
-              )
-            },
-          )}
+          {/* this is a grid that contains 3 images per row */}
+          <ion-grid fixed>
+            <ion-row>
+              {mapArray(
+                get_image_filename_by_id_array(yes.flat()),
+                filename => {
+                  return (
+                    <ion-col size="4">
+                      <ImageItem
+                        image_url={filename.filename}
+                        filename={filename.original_filename}
+                        user={user}
+                      />
+                    </ion-col>
+                  )
+                },
+              )}
+            </ion-row>
+          </ion-grid>
         </ion-segment-content>
         <ion-segment-content id="content-no">
-          {mapArray(
-            get_image_filename_by_id_array(
-              get_image_by_label_id_answer
-                .all({
-                  label_id,
-                  answer: stringToNumber('no'),
-                })
-                .flat(),
-            ),
-            filename => {
-              return (
-                <ImageItem
-                  image_url={filename.filename}
-                  filename={filename.original_filename}
-                  user={user}
-                />
-              )
-            },
-          )}
+          <ion-grid fixed>
+            <ion-row>
+              {mapArray(get_image_filename_by_id_array(no.flat()), filename => {
+                return (
+                  <ion-col size="4">
+                    <ImageItem
+                      image_url={filename.filename}
+                      filename={filename.original_filename}
+                      user={user}
+                    />
+                  </ion-col>
+                )
+              })}
+            </ion-row>
+          </ion-grid>
         </ion-segment-content>
         <ion-segment-content id="content-unknown">
-          {mapArray(
-            get_image_filename_by_id_array(unknown.flat()),
-            filename => {
-              return (
-                <ImageItem
-                  image_url={filename.filename}
-                  filename={filename.original_filename}
-                  user={user}
-                />
-              )
-            },
-          )}
+          <ion-grid fixed>
+            <ion-row>
+              {mapArray(
+                get_image_filename_by_id_array(unknown.flat()),
+                filename => {
+                  return (
+                    <ion-col size="4">
+                      <ImageItem
+                        image_url={filename.filename}
+                        filename={filename.original_filename}
+                        user={user}
+                      />
+                    </ion-col>
+                  )
+                },
+              )}
+            </ion-row>
+          </ion-grid>
         </ion-segment-content>
       </ion-segment-view>
       {/* this is a hidden input that is used to store the answer */}
@@ -387,14 +402,19 @@ function SubmitReview(attrs: {}, context: DynamicContext) {
 
     let unknown_filenames = get_image_filename_by_id_array(unknown.flat())
 
-    let code = /* javascript  */ `
+    let code = /* javascript */ `
       let image_ids = ${JSON.stringify(image_ids)}
       let answer = ${JSON.stringify(answer)}
       let image_filenames = ${JSON.stringify(image_filenames)}
 
       try {
+
+        // get the content of the answer segment
         const content = document.getElementById('content-${answer}');
-        content.innerHTML = '';
+        const grid = content.querySelector('ion-grid');
+        const row = grid.querySelector('ion-row');
+        // clear the row
+        row.innerHTML = '';
 
         if (content.id == 'content-unknown') { 
           image_filenames = ${JSON.stringify(unknown_filenames)}
@@ -402,21 +422,33 @@ function SubmitReview(attrs: {}, context: DynamicContext) {
         
         // it receives an array of filenames and returns a div with the image and the filename (written in javascript)
         image_filenames.forEach(image => {
+
+        const col = document.createElement('ion-col');
+        col.size = '4';
+
+        const item = document.createElement('ion-item');
+        item.className = 'image-item-container';
+
+        const checkbox = document.createElement('ion-checkbox');
          
-          const div = document.createElement('div');
-          div.className = 'image-item';
-          
-          const img = document.createElement('img');
-          img.src = image.filename;
-          
-          const filenameDiv = document.createElement('div');
-          filenameDiv.className = 'image-item--filename';
-          filenameDiv.textContent = image.original_filename;
-          
-          div.appendChild(img);
-          div.appendChild(filenameDiv);
-          content.appendChild(div);
-        });
+        const div = document.createElement('div');
+        div.className = 'image-item';
+        
+        const img = document.createElement('img');
+        img.src = image.filename;
+        
+        const filenameDiv = document.createElement('div');
+        filenameDiv.className = 'image-item--filename';
+        filenameDiv.textContent = image.original_filename;
+        filenameDiv.style.textAlign = 'center';
+        
+        div.appendChild(img);
+        div.appendChild(filenameDiv);
+        item.appendChild(checkbox);
+        item.appendChild(div);
+        col.appendChild(item);
+        row.appendChild(col);
+        })
       } catch (e) {
           console.error('SubmitReview error', e)
       }
