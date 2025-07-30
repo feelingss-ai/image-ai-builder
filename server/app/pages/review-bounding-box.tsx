3c6a9f47f1201ffe4ccfe744aabebd12c6861c09
Ada Lui <luitszhin7@gmail.com>
2025-07-30 17:52:36 +0800

    feat: add label selector, box count selector, and data transmission function

--- server/app/pages/review-bounding-box.tsx
+++ server/app/pages/review-bounding-box.tsx
@@ -7,14 +7,19 @@ import {
   DynamicContext,
   getContextFormBody,
   throwIfInAPI,
+  WsContext,
 } from '../context.js'
 import { mapArray } from '../components/fragment.js'
 import { IonBackButton } from '../components/ion-back-button.js'
-import { object, string } from 'cast.ts'
+import { id, number, object, string } from 'cast.ts'
 import { Link, Redirect } from '../components/router.js'
 import { renderError } from '../components/error.js'
 import { getAuthUser } from '../auth/user.js'
 import { evalLocale, Locale } from '../components/locale.js'
+import { proxy } from '../../../db/proxy.js'
+import { db } from '../../../db/db.js'
+import { Script } from '../components/script.js'
+import { EarlyTerminate } from '../../exception.js'
 
 let pageTitle = (
   <Locale en="Review Bounding Box" zh_hk="審視邊界框" zh_cn="审阅边界框" />
@@ -24,6 +29,45 @@ let style = Style(/* css */ `
 #ReviewBoundingBox {
 
 }
+`)
+
+let script = Script(/* js */ `
+
+  label_select = document.querySelector('#label_select');
+  box_count_select = document.querySelector('#box_count_select');
+
+  label_id = +label_select.value
+  box_count = +box_count_select.value
+  
+  // submit form when label_select is changed
+  label_select.addEventListener('ionChange', (event) => {
+    label_id = event.detail.value;
+  
+    // Update URL with new label parameter
+    const url = new URL(window.location);
+    url.searchParams.set('label', label_id);
+    window.history.pushState({}, '', url);
+    submitBoxCount();
+  })
+
+  box_count_select.addEventListener('ionChange', (event) => {
+    box_count = event.detail.value;
+    // Update URL with new box count parameter
+    const url = new URL(window.location);
+    url.searchParams.set('box_count', box_count);
+    window.history.pushState({}, '', url);
+    submitBoxCount();
+  })
+
+//send selected image label and box count to server
+function submitBoxCount() {
+  console.log('submitBoxCount')
+  emit('/review-bounding-box/submit-box-count', {
+    label_id,
+    box_count,
+  })
+}
+
 `)
 
 let page = (
@@ -37,32 +81,111 @@ let page = (
         </ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content id="TrainAI" class="ion-padding">
-      Items
+    <ion-content id="ReviewBoundingBox" class="ion-padding">
       <Main />
     </ion-content>
+    {script}
   </>
 )
 
-let items = [
-  { title: 'Android', slug: 'md' },
-  { title: 'iOS', slug: 'ios' },
-]
+let count_annotated_images = db
+  .prepare<{ label_id: number }, number>(
+    /* sql */ `
+select count(distinct image_id)
+from image_label
+where label_id = :label_id
+`,
+  )
+  .pluck()
 
-function Main(attrs: {}, context: Context) {
+function Main(attrs: {}, context: DynamicContext) {
   let user = getAuthUser(context)
+  if (!user) {
+    return (
+      <>
+        <div style="margin: auto; width: fit-content; text-align: center;">
+          <p class="ion-padding ion-margin error">
+            <Locale
+              en="You must be logged in to review bounding box"
+              zh_hk="您必須登入才能審視邊界框"
+              zh_cn="您必须登录才能审阅边界框"
+            />
+          </p>
+          <ion-button color="primary" onclick='goto("/login")'>
+            <Locale en="Login" zh_hk="登入" zh_cn="登录" />
+          </ion-button>
+        </div>
+      </>
+    )
+  }
+
+  let params = new URLSearchParams(context.routerMatch?.search)
+  let label_id = +params.get('label')! || 1
+  let total_images = proxy.image.length
 
   return (
     <>
-      <ion-list>
-        {mapArray(items, item => (
-          <ion-item>
-            {item.title} ({item.slug})
-          </ion-item>
-        ))}
-      </ion-list>
+      <ion-item>
+        <ion-select
+          value={label_id}
+          label={Locale(
+            { en: 'Class Label', zh_hk: '類別標籤', zh_cn: '类別标签' },
+            context,
+          )}
+          id="label_select"
+        >
+          {mapArray(proxy.label, label => {
+            let annotated_images = count_annotated_images.get({
+              label_id: label.id!,
+            })
+            return (
+              <ion-select-option value={label.id}>
+                {label.title} ({annotated_images}/{total_images})
+              </ion-select-option>
+            )
+          })}
+        </ion-select>
+      </ion-item>
+      <ion-item>
+        <ion-select
+          label={Locale(
+            {
+              en: 'Box Count',
+              zh_hk: '框數',
+              zh_cn: '框数',
+            },
+            context,
+          )}
+          value={0}
+          id="box_count_select"
+        >
+          <ion-select-option value="0">0 (5)</ion-select-option>
+          <ion-select-option value="1">1 (6)</ion-select-option>
+          <ion-select-option value="2">2 (7)</ion-select-option>
+        </ion-select>
+      </ion-item>
     </>
   )
+}
+
+let submitReviewParser = object({
+  label_id: id(),
+  box_count: number(),
+})
+
+function SubmitReviewBoundingBox(attrs: {}, context: WsContext) {
+  console.log('submitReviewBoundingBox')
+  try {
+    let user = getAuthUser(context)
+    let body = getContextFormBody(context)
+    let input = submitReviewParser.parse(body)
+    let { label_id, box_count } = input
+    console.log('label_id', label_id)
+    console.log('box_count', box_count)
+  } catch (error) {
+    console.error(error)
+  }
+  throw EarlyTerminate
 }
 
 let routes = {
@@ -75,6 +198,12 @@ let routes = {
         node: page,
       }
     },
+  },
+  '/review-bounding-box/submit-box-count': {
+    title: apiEndpointTitle,
+    description: 'TODO',
+    node: <SubmitReviewBoundingBox />,
+    streaming: false,
   },
 } satisfies Routes
 
