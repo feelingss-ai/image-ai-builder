34b3d1952ae71467f3cf57e26da7c44064681130
Ada Lui <luitszhin7@gmail.com>
2025-08-06 11:05:07 +0800

    patch: fix migration down error - use rename instead of drop and add non-null column

--- db/migrations/20250804071042_auto-migrate.ts
+++ db/migrations/20250804071042_auto-migrate.ts
@@ -2,12 +2,10 @@ import { Knex } from 'knex'
 
 // prettier-ignore
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.alterTable(`image_bounding_box`, table => table.dropColumn(`image_label_id`))
-  await knex.raw('alter table `image_bounding_box` add column `label_id` integer not null references `label`(`id`)')
+  await knex.schema.alterTable(`image_bounding_box`, table => table.renameColumn(`image_label_id`, `label_id`))
 }
 
 // prettier-ignore
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.alterTable(`image_bounding_box`, table => table.dropColumn(`label_id`))
-  await knex.raw('alter table `image_bounding_box` add column `image_label_id` integer not null references `image_label`(`id`)')
+  await knex.schema.alterTable(`image_bounding_box`, table => table.renameColumn(`label_id`, `image_label_id`))
 }
