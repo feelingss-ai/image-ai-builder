97b94ebb53d164ca205299a52ac5bf4942f5dd88
Beeno Tung <aabbcc1241@yahoo.com.hk>
2026-03-05 10:56:26 +0800

    seed: include project id when seed label

--- db/seed.ts
+++ db/seed.ts
@@ -9,28 +9,43 @@ seedRow(proxy.method, { method: 'GET' })
 seedRow(proxy.method, { method: 'POST' })
 seedRow(proxy.method, { method: 'ws' })
 
+proxy.user[1] = {
+  username: 'demo',
+  password_hash: null,
+  email: 'demo@example.com',
+  tel: '98765432',
+  avatar: null,
+  is_admin: true,
+  nickname: 'Demo',
+}
+
+proxy.project[1] = {
+  creator_id: 1,
+  title: 'Lobster Pose',
+}
+
 proxy.label[1] = {
   title: '🦞',
   dependency_id: null,
-  project_id: null,
+  project_id: 1,
 }
 proxy.label[2] = {
   title: '🍜',
   dependency_id: null,
-  project_id: null,
+  project_id: 1,
 }
 proxy.label[3] = {
   title: '💩',
   dependency_id: null,
-  project_id: null,
+  project_id: 1,
 }
 proxy.label[4] = {
   title: '開尾',
   dependency_id: 1,
-  project_id: null,
+  project_id: 1,
 }
 proxy.label[5] = {
   title: '舉鉗',
   dependency_id: 1,
-  project_id: null,
+  project_id: 1,
 }
