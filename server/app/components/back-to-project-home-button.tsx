5fdd5f8ffa328fce16ba110568b49d1625ffbac4
Beeno Tung <aabbcc1241@yahoo.com.hk>
2026-03-05 09:26:44 +0800

    patch: fix project id in return link

--- server/app/components/back-to-project-home-button.tsx
+++ server/app/components/back-to-project-home-button.tsx
@@ -4,7 +4,8 @@ import { IonBackButton } from './ion-back-button.js'
 
 export function BackToProjectHomeButton(attrs: {}, context: DynamicContext) {
   let params = new URLSearchParams(context.routerMatch?.search)
-  let project_id = params.get('project_id')
+  // should be using project, not project_id everywhere?
+  let project_id = params.get('project_id') || params.get('project')
   return (
     <IonBackButton
       href={`/app/home?project=${project_id}`}
