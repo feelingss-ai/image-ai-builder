2e23dfb434d8b87ddb55f068261b633052fb93d9
Beeno Tung <aabbcc1241@yahoo.com.hk>
2026-03-05 22:37:07 +0800

    chore: rename back-to-project-home-button.tsx -> project-page-back-button.tsx

--- server/app/components/project-page-back-button.tsx
+++ server/app/components/project-page-back-button.tsx
@@ -1,15 +1,15 @@
 import { DynamicContext } from '../context.js'
+import { getContextProject } from '../context/project-context.js'
 import { o } from '../jsx/jsx.js'
 import { IonBackButton } from './ion-back-button.js'
 
 /** Back to project page when project is in URL, otherwise back to home (project list). */
 export function ProjectPageBackButton(attrs: {}, context: DynamicContext) {
-  let params = new URLSearchParams(context.routerMatch?.search ?? '')
-  let project_id = params.get('project')
-  if (project_id) {
+  let project = getContextProject(context)
+  if (project) {
     return (
       <IonBackButton
-        href={`/app/home?project=${project_id}`}
+        href={`/app/home?project=${project.id}`}
         backText="Project"
       />
     )
